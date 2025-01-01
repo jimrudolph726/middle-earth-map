772c6add1287890ec688e74b41b617775116681c
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-01 12:54:09 -0600

    d

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Initialize the map
 const map = L.map('map', {
     crs: L.CRS.Simple, // Use simple coordinate reference system for flat maps
-    minZoom: -40, // Allow zooming out more
+    minZoom: -5, // Allow zooming out more
     maxZoom: -1, // You can adjust this to restrict zooming in
     zoom: -5, // Set the initial zoom level
   });
