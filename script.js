e466f28977d82ddac80a5ba4003c9fb669ea7478
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-01 17:39:27 -0600

    d

--- script.js
+++ script.js
@@ -1,143 +1,111 @@
 // script.js
 
-const HobbitsIcon = L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/hobbits.png', // Path to your custom image
-  iconSize: [48, 48],  // Size of the icon (adjust as needed)
-  iconAnchor: [16, 32], // Point of the icon that will be at the marker's location
-  popupAnchor: [0, -32] // Position of the popup relative to the icon
+// Function to create icons
+const createIcon = (url) => L.icon({
+  iconUrl: url,
+  iconSize: [48, 48],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
 });
-const MenIcon = L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/men.png', // Path to your custom image
-  iconSize: [48, 48],  // Size of the icon (adjust as needed)
-  iconAnchor: [16, 32], // Point of the icon that will be at the marker's location
-  popupAnchor: [0, -32] // Position of the popup relative to the icon
-});
-const RivendellIcon = L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/rivendell.png', // Path to your custom image
-  iconSize: [48, 48],  // Size of the icon (adjust as needed)
-  iconAnchor: [16, 32], // Point of the icon that will be at the marker's location
-  popupAnchor: [0, -32] // Position of the popup relative to the icon
-});
+
+// Create marker icons
+const HobbitsIcon = createIcon('https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/hobbits.png');
+const MenIcon = createIcon('https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/men.png');
+const RivendellIcon = createIcon('https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/rivendell.png');
 
 // Initialize the map
 const map = L.map('map', {
-    crs: L.CRS.Simple, // Use simple coordinate reference system for flat maps
-    minZoom: -3, // Allow zooming out more
-    maxZoom: 2, // You can adjust this to restrict zooming in
-    zoom: -3, // Set the initial zoom level
-  });
-  
-  // Image dimensions (width and height in pixels)
-  const imageWidth = 8740; // Adjust to your image's width
-  const imageHeight = 8208; // Adjust to your image's height
-  
-  // Define the image bounds ([top-left corner, bottom-right corner])
-  const imageBounds = [[0, 0], [imageHeight, imageWidth]];
-  
-  // Add the image as a map layer
-  const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/middle-earth.png'; // Path to your image file
-  L.imageOverlay(imageUrl, imageBounds).addTo(map);
+  crs: L.CRS.Simple,
+  minZoom: -3,
+  maxZoom: 2,
+  zoom: -3,
+});
 
-  // Add the image as a map layer
-  const SamFrodoPathUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/samfrodopath.png'; // Path to your image file
-  const SamFrodoPathOverlay = L.imageOverlay(SamFrodoPathUrl, imageBounds);
+// Image dimensions and bounds
+const imageWidth = 8740;
+const imageHeight = 8208;
+const imageBounds = [[0, 0], [imageHeight, imageWidth]];
 
-  // Set the view to fit the image
-  map.fitBounds(imageBounds);
-  
+// Add the image as a map layer
+const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/middle-earth.png';
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+// Add the path overlay
+const SamFrodoPathUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/samfrodopath.png';
+const SamFrodoPathOverlay = L.imageOverlay(SamFrodoPathUrl, imageBounds);
+
+// Set the view to fit the image
+map.fitBounds(imageBounds);
+
 // Function to convert the Y-coordinate
-  function convertYCoordinate(y) {
-    return imageHeight - y;
+function convertYCoordinate(y) {
+  return imageHeight - y;
+}
+
+// Create markers and their coordinates
+const locations = {
+  hobbiton: { coords: [convertYCoordinate(2329), 2247], icon: HobbitsIcon, popup: 'Hobbiton' },
+  micheldelving: { coords: [convertYCoordinate(2388), 2110], icon: HobbitsIcon, popup: 'Michel Delving' },
+  bree: { coords: [convertYCoordinate(2251), 2794], icon: MenIcon, popup: 'Bree' },
+  rivendell: { coords: [convertYCoordinate(2244), 4240], icon: RivendellIcon, popup: `<div><h3>Rivendell</h3><button onclick="window.open('https://thainsbook.minastirith.cz/rivendell.html', '_blank')" style="cursor: pointer; padding: 5px 10px; background-color: #007bff; color: white; border: none; border-radius: 5px;">Learn more on Thain's Book</button></div>` },
+  minastirith: { coords: [convertYCoordinate(5113), 5726], icon: MenIcon, popup: 'Minas Tirith' }
+};
+
+// Add markers to the map
+const markers = Object.keys(locations).reduce((acc, key) => {
+  const { coords, icon, popup } = locations[key];
+  const marker = L.marker(coords, { icon }).bindPopup(popup);
+  acc[key] = marker;
+  return acc;
+}, {});
+
+// Define the path (polyline)
+const pathCoordinates = [
+  locations.hobbiton.coords, 
+  locations.bree.coords, 
+  locations.rivendell.coords
+];
+const fellowshipPath = L.polyline(pathCoordinates, { color: 'blue' });
+
+// Add toggle button functionality
+function toggleVisibility(layer, button, state) {
+  if (state) {
+    map.removeLayer(layer);
+    button.innerHTML = `Show ${layer.options.icon.options.iconUrl.split('/').pop().split('.')[0]}`;
+  } else {
+    layer.addTo(map);
+    button.innerHTML = `Hide ${layer.options.icon.options.iconUrl.split('/').pop().split('.')[0]}`;
   }
-  
-  // Set up markers using the convertYCoordinate function with raw Y-values
-  const hobbitonMarker = L.marker([convertYCoordinate(2329), 2247], { icon: HobbitsIcon })// Hobbiton
-    .bindPopup('Hobbiton');
-  const micheldelvingMarker = L.marker([convertYCoordinate(2388), 2110], { icon: HobbitsIcon }) // Hobbiton
-  .bindPopup('Michel Delving');
-  
-  const breeMarker = L.marker([convertYCoordinate(2251), 2794], { icon: MenIcon }) // Bree
-    .bindPopup('Bree');
-  
-  const rivendellMarker = L.marker([convertYCoordinate(2244), 4240], { icon: RivendellIcon }) // Rivendell
-  .bindPopup(`
-      <div>
-          <h3>Rivendell</h3>
-          <button onclick="window.open('https://thainsbook.minastirith.cz/rivendell.html', '_blank')" style="cursor: pointer; padding: 5px 10px; background-color: #007bff; color: white; border: none; border-radius: 5px;">Learn more on Thain's Book</button>
-      </div>
-  `);
+  return !state;
+}
 
-  const minastirithMarker = L.marker([convertYCoordinate(5113), 5726], { icon: MenIcon }) // Minas Tirith
-    .bindPopup('Minas Tirith');
+// Initialize toggle states
+let visibilityState = {
+  hobbits: false,
+  men: false,
+  elves: false,
+  samFrodoPath: false
+};
 
-  // Define the path (polyline)
-  const pathCoordinates = [
-    [convertYCoordinate(2329), 2247], // Hobbiton
-    [convertYCoordinate(2251), 2794], // Bree
-    [convertYCoordinate(2244), 4240]  // Rivendell
-  ];
-  
-  const fellowshipPath = L.polyline(pathCoordinates, { color: 'blue' });
+// Toggle buttons for markers and path
+const toggleHobbitsButton = document.getElementById('toggleHobbitsButton');
+toggleHobbitsButton.addEventListener('click', () => {
+  visibilityState.hobbits = toggleVisibility(markers.hobbiton, toggleHobbitsButton, visibilityState.hobbits);
+  visibilityState.micheldelving = toggleVisibility(markers.micheldelving, toggleHobbitsButton, visibilityState.micheldelving);
+});
 
-  // Toggle button functionality for Hobbits
-  let isHobbitonVisible = false; // Initially, Hobbiton marker is visible
-  const toggleHobbitsButton = document.getElementById('toggleHobbitsButton');
+const toggleMenButton = document.getElementById('toggleMenButton');
+toggleMenButton.addEventListener('click', () => {
+  visibilityState.men = toggleVisibility(markers.minastirith, toggleMenButton, visibilityState.men);
+  visibilityState.bree = toggleVisibility(markers.bree, toggleMenButton, visibilityState.bree);
+});
 
-  toggleHobbitsButton.addEventListener('click', () => {
-    if (isHobbitonVisible) {
-      map.removeLayer(hobbitonMarker); // Remove Hobbiton marker from the map
-      map.removeLayer(micheldelvingMarker);  
-      toggleHobbitsButton.innerHTML = 'Show Hobbits';  // Change button text
-    } else {
-      hobbitonMarker.addTo(map);  // Add Hobbiton marker to the map
-      micheldelvingMarker.addTo(map);
-      toggleHobbitsButton.innerHTML = 'Hide Hobbits';  // Change button text
-    }
-    isHobbitonVisible = !isHobbitonVisible;
-  });
+const toggleElvesButton = document.getElementById('toggleElvesButton');
+toggleElvesButton.addEventListener('click', () => {
+  visibilityState.elves = toggleVisibility(markers.rivendell, toggleElvesButton, visibilityState.elves);
+});
 
-    // Toggle button functionality for Hobbits
-    let isMinasTirithVisible = false; // Initially, Hobbiton marker is visible
-    const toggleMenButton = document.getElementById('toggleMenButton');
-  
-    toggleMenButton.addEventListener('click', () => {
-      if (isMinasTirithVisible) {
-        map.removeLayer(minastirithMarker); // Remove Hobbiton marker from the map 
-        map.removeLayer(breeMarker); 
-        toggleMenButton.innerHTML = 'Show Men';  // Change button text
-      } else {
-        minastirithMarker.addTo(map);  // Add Hobbiton marker to the map
-        breeMarker.addTo(map); 
-        toggleMenButton.innerHTML = 'Hide Men';  // Change button text
-      }
-      isMinasTirithVisible = !isMinasTirithVisible;
-    });
-
-    
-    // Toggle button functionality for Hobbits
-    let isRivendellVisible = false; // Initially, Hobbiton marker is visible
-    const toggleElvesButton = document.getElementById('toggleElvesButton');
-    toggleElvesButton.addEventListener('click', () => {
-      if (isRivendellVisible) {
-        map.removeLayer(rivendellMarker); // Remove Hobbiton marker from the map 
-        toggleElvesButton.innerHTML = 'Show Elves';  // Change button text
-      } else {
-        rivendellMarker.addTo(map);  // Add Hobbiton marker to the map
-        toggleElvesButton.innerHTML = 'Hide Elves';  // Change button text
-      }
-      isRivendellVisible = !isRivendellVisible;
-    });
-
-    // Toggle button functionality for Hobbits
-    let SamFrodoPathVisible = false; // Initially, Hobbiton marker is visible
-    const toggleSamFrodoPathButton = document.getElementById('toggleSamFrodoPathButton');
-    toggleSamFrodoPathButton.addEventListener('click', () => {
-      if (SamFrodoPathVisible) {
-        map.removeLayer(SamFrodoPathOverlay); // Remove Hobbiton marker from the map 
-        toggleSamFrodoPathButton.innerHTML = "Show Sam and Frodo's Path";  // Change button text
-      } else {
-        SamFrodoPathOverlay.addTo(map);  // Add Hobbiton marker to the map
-        toggleSamFrodoPathButton.innerHTML = "Hide Sam and Frodo's Path";  // Change button text
-      }
-      SamFrodoPathVisible = !SamFrodoPathVisible;
-    });
+const toggleSamFrodoPathButton = document.getElementById('toggleSamFrodoPathButton');
+toggleSamFrodoPathButton.addEventListener('click', () => {
+  visibilityState.samFrodoPath = toggleVisibility(SamFrodoPathOverlay, toggleSamFrodoPathButton, visibilityState.samFrodoPath);
+});
