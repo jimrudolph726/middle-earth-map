71aeadb8dce70da1feebe06209f986dd2c10bd28
jimrudolph726 <130412104+jimrudolph726@users.noreply.github.com>
2024-12-31 22:04:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add the georeferenced map image overlay
-L.imageOverlay('https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/middle-earth.tif', 
+L.imageOverlay('https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/middle-earth.jpg', 
                [[-60, -150], [60, 150]])  // Adjust the bounds to match your image
     .addTo(map);
 
