1bb968868fca74c71496f08f624634f7bcb1497b
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-01 19:54:12 -0600

    d

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function convertYCoordinate(y) {
 // Create markers and their coordinates
 const locations = {
   hobbiton: { coords: [convertYCoordinate(2329), 2247], icon: HobbitsIcon, popup: 'Hobbiton' },
-  micheldelving: { coords: [convertYCoordinate(2388), 2110], icon: HobbitsIcon, popup: 'Michel Delving' },
+  micheldelving: { coords: [convertYCoordinate(2388), 2110], icon: HobbitsIcon, popup: `<div><h3>Michel Delving</h3><button onclick="window.open('https://thainsbook.minastirith.cz/towns.html#Michel%20Delving', '_blank')" style="cursor: pointer; padding: 5px 10px; background-color: #007bff; color: white; border: none; border-radius: 5px;">Learn more on Thain's Book</button></div>` },
   bree: { coords: [convertYCoordinate(2251), 2794], icon: MenIcon, popup: 'Bree' },
   rivendell: { coords: [convertYCoordinate(2244), 4240], icon: RivendellIcon, popup: `<div><h3>Rivendell</h3><button onclick="window.open('https://thainsbook.minastirith.cz/rivendell.html', '_blank')" style="cursor: pointer; padding: 5px 10px; background-color: #007bff; color: white; border: none; border-radius: 5px;">Learn more on Thain's Book</button></div>` },
   minastirith: { coords: [convertYCoordinate(5113), 5726], icon: MenIcon, popup: 'Minas Tirith' }
