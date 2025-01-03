b416c59dca3e8505f4d37d7a98867b8774cb3bdc
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-02 20:13:27 -0600

    d

--- functions.js
+++ functions.js
@@ -34,26 +34,21 @@ export const createPaths = (data, bounds) => {
   return overlays;
 };
 
-export const addCheckboxListenerSingle = (checkboxId, marker, map) => {
-  const checkbox = document.getElementById(checkboxId);
-  if (checkbox) {
-    checkbox.addEventListener('change', (event) => {
+export const addCheckboxListenerSingle = (checkboxId, element, map) => {
+  document.getElementById(checkboxId).addEventListener('change', (event) => {
       if (event.target.checked) {
-        // Add the marker to the map
-        marker.addTo(map);
+          // Add the element (marker or overlay) to the map
+          element.addTo(map);
       } else {
-        // Remove the marker from the map
-        map.removeLayer(marker);
+          // Remove the element (marker or overlay) from the map
+          map.removeLayer(element);
       }
-    });
-  }
+  });
 };
 
-export const addCheckboxListenerMultiple = (checkboxIds, markers, map) => {
-  checkboxIds.forEach((checkboxId) => {
-    const marker = markers[checkboxId];
-    if (marker) {
-      addCheckboxListenerSingle(checkboxId, marker, map);
-    }
+export const addCheckboxListenerMultiple = (checkboxId, markers, map) => {
+  Object.values(markers).forEach((marker) => {
+    // Send each marker and checkboxId to addCheckboxListener
+    addCheckboxListenerSingle(checkboxId, marker, map);
   });
 };
