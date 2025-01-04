56ba15aa23974928ec2dd462a483ffc0645324d1
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-04 15:21:13 -0600

    d

--- script.js
+++ script.js
@@ -11,46 +11,13 @@ const map = L.map('map', {
   minZoom: -3,
   maxZoom: 2,
   zoom: -3,
-});
+},
+{drawControl: true});
 // Add the image as a map layer
 const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/middle-earth.png';
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 // Set the view to fit the image
 map.fitBounds(imageBounds);
-
-
-
-
-
-
-// Add Leaflet.draw functionality
-var drawControl = new L.Control.Draw({
-  draw: {
-      polyline: true, // Enable polyline drawing
-      polygon: false, // Disable polygon drawing
-      rectangle: false, // Disable rectangle drawing
-      circle: false, // Disable circle drawing
-      marker: false // Disable marker drawing
-  },
-  edit: {
-      featureGroup: drawnItems // The feature group for editing drawn items
-  }
-});
-map.addControl(drawControl);
-
-// Create a feature group to store drawn items
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
-
-// Event listener for when a user finishes drawing a line
-map.on('draw:created', function (e) {
-  var layer = e.layer;
-  drawnItems.addLayer(layer); // Add the drawn line to the feature group
-});
-
-
-
-
 
 // Create markers
 const markers = createMarkers(locations);
