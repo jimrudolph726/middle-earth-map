96ad6b03fb385e30ccd8cd4044c938fcbeff3db6
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-04 21:29:56 -0600

    d

--- script.js
+++ script.js
@@ -31,21 +31,13 @@ const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-m
 const imageBounds = [[50.9384172749999991, -116.1398198509999986], [27.2271753850000024, -82.8417954509999959]];
 
 // Add the image as an overlay to the map
-L.imageOverlay(imageUrl, imageBounds).addTo(map);
+const imageOverlay = L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+// Apply the CSS to visually compress the image horizontally
+imageOverlay.getElement().style.transform = 'scaleX(0.8)'; // Adjust the 0.8 value as needed
 
 // Optionally, fit the map view to the bounds of the image
 map.fitBounds(imageBounds);
-
-
-
-
-
-
-
-
-
-
-
 
 
 
