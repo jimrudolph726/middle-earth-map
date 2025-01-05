d93beeba85074f48cbe3109d58d61f4865fcbdce
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-04 20:10:07 -0600

    d

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ import {
 } from './functions.js';
 import {
   locations,
-  imageBounds,
   pathsData,
   hobbitlocations,
   samfrodosteps,
@@ -24,22 +23,59 @@ const map = L.map('map', {
   center: [0, 0], // Adjust to the approximate center of your raster if needed
 });
 
-fetch('https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/middle-earth-tif.tif') // Replace with your GeoTIFF file URL
-  .then(response => response.arrayBuffer())
-  .then(arrayBuffer => {
-    parseGeoraster(arrayBuffer).then(georaster => {
-      console.log("Parsed GeoRaster:", georaster);
-      const layer = new GeoRasterLayer({
-        georaster,
-        opacity: 1, // Adjust transparency
-        resolution: 1024, // Adjust quality
-      });
-      layer.addTo(map);
 
-      map.fitBounds(layer.getBounds());
-    });
-  })
-  .catch(error => console.error("Error parsing GeoRaster:", error));
+// Define the URL of your image (the .tif file)
+const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/middle-earth-tif.tif';
+
+// Define the bounds of your image in geographic coordinates (latitude/longitude)
+// You need to specify the coordinates of the top-left and bottom-right corners of the image
+const imageBounds = [[50.9384172749999991, -116.1398198509999986], [27.2271753850000024, -82.8417954509999959]];
+
+// Add the image as an overlay to the map
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+// Optionally, fit the map view to the bounds of the image
+map.fitBounds(imageBounds);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch('https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/middle-earth-tif.tif') // Replace with your GeoTIFF file URL
+//   .then(response => response.arrayBuffer())
+//   .then(arrayBuffer => {
+//     parseGeoraster(arrayBuffer).then(georaster => {
+//       console.log("Parsed GeoRaster:", georaster);
+//       const layer = new GeoRasterLayer({
+//         georaster,
+//         opacity: 1, // Adjust transparency
+//         resolution: 1024, // Adjust quality
+//       });
+//       layer.addTo(map);
+
+//       map.fitBounds(layer.getBounds());
+//     });
+//   })
+//   .catch(error => console.error("Error parsing GeoRaster:", error));
+
+
+
+
 
 // Create markers and paths
 const markers = createMarkers(locations);
