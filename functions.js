3c35371bf13e75f08a8e04533596ad2ffceb30e1
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-05 07:59:48 -0600

    d

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@
     return L.icon({
       iconUrl: url,
       iconSize: [48, 48],  // Adjust size as needed
-      iconAnchor: [16, 32],
+      iconAnchor: [32, 32],
       popupAnchor: [0, -32],
     });
   }
