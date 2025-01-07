c1bf1e8e2a67827b8f579dcdb5862822d28f9f53
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-07 17:26:56 -0600

    d

--- script.js
+++ script.js
@@ -54,9 +54,10 @@ addCheckboxListenerSingle('elvesCheckbox', markers['rivendell'], map);
 addCheckboxListenerMultiple('samfrodopathCheckbox', samFrodoMarkers, map);
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const pathsraw = { samfrodopath: { pathName: 'samfrodopath', color: 'red', map: map },
-                             test: { pathName: 'test', color: 'blue', map: map }};
+  // const pathsraw = { samfrodopath: { pathName: 'samfrodopath', color: 'red', map: map },
+  //                            test: { pathName: 'test', color: 'blue', map: map }};
 
+  const pathsraw = { samfrodopath: { pathName: 'samfrodopath', color: 'red', map: map }};
   console.log('Initializing polylines...');
   const polylines = await createPolyline(pathsraw); // Wait for polylines to be ready
   console.log('Polylines initialized:', polylines);
