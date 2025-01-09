bbee8ef6d22dbd8cf2bb8f36d3b7933d05e1b2cd
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-08 20:47:18 -0600

    d

--- functions.js
+++ functions.js
@@ -1,24 +1,13 @@
 // functions.js
 
 export const createlocationMarkers = (locations) => {
-  return new Promise((resolve, reject) => {
-    try {
-      const markers = Object.keys(locations).reduce((acc, key) => {
-        const { coords, icon, popup } = locations[key];
-        const marker = L.marker(coords, { icon }).bindPopup(popup);
-        acc[key] = marker;
-        return acc;
-      }, {});
-      
-      // Resolve the promise once the markers are created
-      resolve(markers);
-    } catch (error) {
-      // Reject the promise if there's an error
-      reject(error);
-    }
-  });
+  return Object.keys(locations).reduce((acc, key) => {
+    const { coords, icon, popup } = locations[key];
+    const marker = L.marker(coords, { icon }).bindPopup(popup);
+    acc[key] = marker;
+    return acc;
+  }, {});
 };
-
 
 export const createpathMarkers = (locations) => {
   return Object.keys(locations).reduce((acc, key) => {
