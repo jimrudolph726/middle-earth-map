c1b5d17e879a3b2ee012e671fa225fd14ce0f3c8
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-08 22:03:35 -0600

    d

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ import {
   menlocations,
   hobbitlocations,
   samfrodocampsites,
+  pathdata,
+  mountain_ranges
 } from './variables.js';
 
-// Map
+// Add Map
 const map = L.map('map', {
   crs: L.CRS.EPSG3857,
   minZoom: -5,
@@ -29,26 +31,20 @@ const map = L.map('map', {
 const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/output_file.png';
 const imageBounds = [[44.95133395351252, -93.31776393673807],[44.93460911676505, -93.29255872642499],];
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
-
 map.fitBounds(imageBounds);
 
-// Create markers
+// Add Paths
+createPolyline(pathdata).then((polylines) => {addCheckboxListeners(polylines, map);});
+
+// Add Locations
 const elvesMarkers = createlocationMarkers(elveslocations);
 const menMarkers = createlocationMarkers(menlocations);
 const hobbitMarkers = createlocationMarkers(hobbitlocations);
 const samfrodopathMarkers = createpathMarkers(samfrodocampsites);
-
-// Add event listeners for checkboxes
 addCheckboxListenerMultiple('elvesCheckbox', elvesMarkers, map);
 addCheckboxListenerMultiple('menCheckbox', menMarkers, map);
 addCheckboxListenerMultiple('hobbitsCheckbox', hobbitMarkers, map);
 addCheckboxListenerMultiple('datesCheckbox', samfrodopathMarkers, map);
 
-// Add paths
-const pathdata = { samfrodopath: { pathName: 'samfrodopath', color: 'red', map: map },
-                        aragorn: { pathName: 'aragorn', color: 'blue', map: map }};
-createPolyline(pathdata).then((polylines) => {addCheckboxListeners(polylines, map);});
-
-// Add polygons
-const mountain_ranges = {misty_mountains: {mountain_range_name:'misty_mountains', color: 'orange', map: map },}
+// Add Geographic Features
 createPolygon(mountain_ranges).then((polygons) => {addCheckboxListeners(polygons, map);});
