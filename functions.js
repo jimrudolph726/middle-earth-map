b44ccc417467778bbb92b2183695ff8e55ce64ad
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-09 18:47:24 -0600

    d

--- functions.js
+++ functions.js
@@ -76,7 +76,8 @@ export const addCheckboxListenerMultiple = (checkboxId, markers, map) => {
 };
 export const addCheckboxListeners = (items, map) => {
   Object.keys(items).forEach((key) => {
-    const checkbox = document.getElementById(`${key}Checkbox`);
+    const CheckboxId = items[key].CheckboxId;
+    const checkbox = document.getElementById(`${CheckboxId}Checkbox`);
     console.log(`Checkbox for ${key}:`, checkbox); // Debug checkbox
     if (checkbox) {
       checkbox.addEventListener('change', (event) => {
