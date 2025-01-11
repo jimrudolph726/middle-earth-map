88958436d35db57da42140ec4984ea451385fe08
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-11 11:04:21 -0600

    d

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ const map = L.map('map', {
   center: [0, 0],
 });
 
-var sidebar = L.control.sidebar('sidebar').addTo(map);
-
 const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/output_file.png';
 const imageBounds = [[44.95133395351252, -93.31776393673807],[44.93460911676505, -93.29255872642499],];
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 map.fitBounds(imageBounds);
+
+var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 // Add Paths and Campsites
 createPolyline(pathdata).then((polylines) => {PathListeners(polylines, map);});
