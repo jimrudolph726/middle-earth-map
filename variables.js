d3f66acecd81ed01a2786ab5ee9d5770dace282f
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-12 12:53:22 -0600

    d

--- variables.js
+++ variables.js
@@ -28,7 +28,7 @@ const icons = {
 export const pathdata = { 
   samfrodopath: { pathName: 'samfrodopath', color: 'red' }, 
   aragornpath: { pathName: 'aragornpath', color: 'blue' },
-  pippinpath: { pathName: 'pippinpathdissolvedtest', color: 'green' },
+  pippinpath: { pathName: 'pippinpathdissolvedtest2', color: 'green' },
 
 };
 export const samfrodocampsites = {
