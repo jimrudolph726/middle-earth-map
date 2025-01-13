578b4cd02f03dbccf5885b741a53b760319cd4b3
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-12 22:30:36 -0600

    d

--- forest.js
+++ forest.js
@@ -1,8 +1,7 @@
 export const forest = {
-    mirkwood: {
-      name: "Mirkwood",
-      description: "Largest continuous forest in Middle-earth during the War of the Ring. King Thranduil's kingdom is located here.",
-      url: 'https://thainsbook.minastirith.cz/mirkwood.html',
-    }
-  }
-  
+  mirkwood: {
+    name: "Mirkwood",
+    description: "Largest continuous forest in Middle-earth during the War of the Ring. King Thranduil's kingdom is located here.",
+    url: "https://thainsbook.minastirith.cz/mirkwood.html",
+  },
+};
