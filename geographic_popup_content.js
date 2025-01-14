a83f546f4fe13176daa1ff852f64d2c67b594e9d
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-14 06:35:15 -0600

    d

--- geographic_popup_content.js
+++ geographic_popup_content.js
@@ -5,13 +5,6 @@ export const forest = {
     elvish_meaning: `
       Taur-nu-Fuin means "forest under night." 
       The word taur means "wood, forest"; nu means "down below, underneath"; and fuin means "gloom, darkness."
-
-      Taur-e-Ndaedelos means "forest of great fear." 
-      The word daedelos means "shadow of abomination" or "shadow of fear." 
-      The element dae means "shadow" and delos means "abhorrence, detestation, loathing" from dyel meaning "feel fear and disgust; abhor."
-
-      Eryn Galen means "green wood" from eryn meaning "wood" and galen meaning "green." 
-      Eryn Lasgalen means "wood of green leaves"; the word las means "leaf."
     `,
     description: "Largest continuous forest in Middle-earth during the War of the Ring. King Thranduil's kingdom is located here.",
     url: "https://thainsbook.minastirith.cz/mirkwood.html",
