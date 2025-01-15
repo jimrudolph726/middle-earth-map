01fcd27bc8db32e0844b14bbe4c62d86853c6235
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-15 09:55:48 -0600

    d

--- geographic_popup_content.js
+++ geographic_popup_content.js
@@ -18,6 +18,6 @@ export const mountain = {
     elvish_name: "Hithaeglir",
     elvish_meaning: 'The name Hithaeglir means "Line of Misty Peaks" from hîth meaning "mist" and oeglir meaning "range of mountain peaks."',
     description: "Largest continuous forest in Middle-earth during the War of the Ring. King Thranduil's kingdom is located here.",
-    url: "https://thainsbook.minastirith.cz/mirkwood.html",
+    url: "https://thainsbook.minastirith.cz/mountains.html#Misty",
   },
 };
