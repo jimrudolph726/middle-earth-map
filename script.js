8dd5211511e55baabdf9ac363a7e41c200962c68
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-20 16:42:54 -0600

    d

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ import {
 // Add Mapd
 const map = L.map('map', {
   crs: L.CRS.EPSG3857,
-  minZoom: 15.5,
+  minZoom: 15,
   maxZoom: 20,
   zoom: 15.5, // Fractional zoom level
   center: [0, 0],
