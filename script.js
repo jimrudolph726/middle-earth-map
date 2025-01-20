6895a1628b18c96d15844934a6e1b15ca44ec4cc
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-20 16:40:08 -0600

    d

--- script.js
+++ script.js
@@ -19,9 +19,12 @@ const map = L.map('map', {
   crs: L.CRS.EPSG3857,
   minZoom: 15,
   maxZoom: 20,
-  zoom: 15,
+  zoom: 15.5, // Fractional zoom level
   center: [0, 0],
+  zoomSnap: 0.1, // Allows fractional zoom levels
+  zoomDelta: 0.1, // Controls the increment of zoom changes
 });
+
 
 const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/middle-earth.png';
 const imageBounds = [[44.95133395351252, -93.31776393673807],[44.93460911676505, -93.29255872642499],];
