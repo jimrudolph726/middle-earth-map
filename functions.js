459b9a40d78082a653bdc6d276b68ced494700d3
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-20 12:51:47 -0600

    d

--- functions.js
+++ functions.js
@@ -157,6 +157,9 @@ export const createPolyline = async (paths) => {
         size: '20px',       // Size of the arrows
         frequency: '20px',   // Frequency of arrows along the path
         yawn: 30,           // Width of the opening of the arrowhead
+        fill: true,
+        color: black,
+        fillColor: color,
       });
 
       polylines[key] = polyline;
