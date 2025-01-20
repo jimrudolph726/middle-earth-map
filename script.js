fda1acd6f8a82dc8e40f948b89a805260c20a136
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-20 16:46:59 -0600

    d

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ import {
   geographicData
 } from './variables.js';
 
-// Add Mapd
+// Add Map
 const map = L.map('map', {
   crs: L.CRS.EPSG3857,
   minZoom: 15,
@@ -22,7 +22,7 @@ const map = L.map('map', {
   zoom: 15.5, // Fractional zoom level
   center: [0, 0],
   zoomSnap: 0.1, // Allows fractional zoom levels
-  zoomDelta: 1, // Controls the increment of zoom changes
+  zoomDelta: 2, // Controls the increment of zoom changes
 });
 
 
