d01d15c1f5d1047cdf2c93c1d9fc511519d98234
jimrudolph726 <jimrudolph726@gmail.com>
2025-01-20 11:58:09 -0600

    d

--- functions.js
+++ functions.js
@@ -156,7 +156,8 @@ export const createPolyline = async (paths) => {
       const polyline = L.polyline(latLngs, { color, weight: 5, opacity: 0.8 }).arrowheads({
         size: '20px',       // Size of the arrows
         frequency: 20,   // Frequency of arrows along the path
-        angle: 30           // Angle of the arrows
+        angle: 30,           // Angle of the arrows
+        interpolateOnLine: true
       });
       polylines[key] = polyline;
       console.log(`Polyline created and added for ${key}`);
