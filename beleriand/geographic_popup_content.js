8e3fc1268cddf97e724ff524046fe4692e6653c1
jimrudolph726 <jimrudolph726@gmail.com>
2025-02-12 13:05:28 -0600

    d

---
+++ beleriand/geographic_popup_content.js
@@ -0,0 +1,83 @@
+export const forest = {
+  mirkwood: {
+    name: "Mirkwood",
+    elvish_name: "Taur-nu-Fuin",
+    elvish_meaning: `
+      Taur-nu-Fuin means "forest under night." 
+      The word taur means "wood, forest"; nu means "down below, underneath"; and fuin means "gloom, darkness."
+    `,
+    description: "Largest continuous forest in Middle-earth during the War of the Ring. King Thranduil's kingdom is located here.",
+    url: "https://tolkiengateway.net/wiki/Mirkwood",
+  },
+  old_forest: {
+    name: "The Old Forest",
+    elvish_name: "(speculative) Iaur Taur",
+    elvish_meaning: `
+      The name Iaur Taur means "old forest" or "ancient forest" from the word iaur meaning old or ancient, and taur meaning forest.  This is purely speculative and noncanonical.
+    `,
+    description: "Forest on the east side of the Brandywine River bordering Buckland.",
+    url: "https://tolkiengateway.net/wiki/Old_Forest",
+  },
+
+  blackwood: {
+    name: "The Blackwood",
+    elvish_name: "Eryn Vorn",
+    elvish_meaning: `
+      The name Eryn Vorn means "Dark Wood" from the word eryn meaning "wood" and vorn from morn meaning "dark."
+    `,
+    description: "The Blackwood was a vast ancient treescape that covered most of north-western Middle-earth. It was named 'The Black Wood' by the Númenóreans during the Second Age.",
+    url: "https://tolkiengateway.net/wiki/Eryn_Vorn",
+  },
+
+
+
+
+
+
+
+};
+
+export const mountain = {
+  misty_mountains: {
+    name: "Misty Mountains",
+    elvish_name: "Hithaeglir",
+    elvish_meaning: 'The name Hithaeglir means "Line of Misty Peaks" from hîth meaning "mist" and oeglir meaning "range of mountain peaks."',
+    description: "Largest continuous forest in Middle-earth during the War of the Ring. King Thranduil's kingdom is located here.",
+    url: "https://tolkiengateway.net/wiki/Misty_Mountains",
+  },
+  white_mountains: {
+    name: "White Mountains",
+    elvish_name: "Ered Nimrais",
+    elvish_meaning: 'The White Mountains were called Ered Nimrais in Sindarin meaning "White-horn Mountains." The word ered means "mountains" from the singular orod. The word nimrais means "white horns" from nim meaning "white" and rais, the plural of ras meaning "horn." The mountains were named for their snow-capped peaks.',
+    description: "Mountain range of Gondor and Rohan. The White Mountains were high and their peaks were covered with snow year-round. ",
+    url: "https://tolkiengateway.net/wiki/White_Mountains",
+  },
+  ash_mountains: {
+    name: "Ash Mountains",
+    elvish_name: "Ered Lithui",
+    elvish_meaning: 'Also called the Ashen Mountains. The Ash Mountains were called Ered Lithui in Sindarin. The word ered means "mountains" and lithui means "ashen." They were so called because of their ash-grey color.',
+    description: "Mountain range on the northern border of Mordor. The Ash Mountains stretched 500 miles from east to west. ",
+    url: "https://tolkiengateway.net/wiki/Ered_Lithui",
+  },
+  iron_hills: {
+    name: "Iron Hills",
+    elvish_name: "Emyn Angren",
+    elvish_meaning: 'The Iron Hills were so called because they were rich in iron ore. The Sindarin name was probably Emyn Engrin from emyn meaning "hills" and engrin the plural of angren meaning "iron".',
+    description: "Range of hills in northeastern Middle-earth. The Iron Hills were isolated in the middle of an empty region about 125 miles east of the Lonely Mountain and Mirkwood. The range stretched about 200 miles from west to east. Near the western end, the Redwater flowed southward from the hills to join the River Running. On the eastern end, a spur of hills curved north and then back westward forming a hook at the end of the range.",
+    url: "https://tolkiengateway.net/wiki/Iron_Hills",
+  },
+  lonely_mountain: {
+    name: "Lonely Mountain",
+    elvish_name: "Erebor",
+    elvish_meaning: 'The Lonely Mountain was so called because it stood alone and was not part of a mountain range. The name Erebor means "Lonely Mountain." The word ere means "be alone" and ereb means "isolated." The element oro means "up, rise, high." The Dwarf realm in the Lonely Mountain was called the Kingdom under the Mountain.',
+    description: "Mountain realm of the Dwarves captured by Smaug the Dragon. The Lonely Mountain was a tall, isolated mountain in the far northeast of Wilderland. It stood near the edge of Mirkwood, about 50 miles south of the Grey Mountains and about 125 miles west of the Iron Hills.",
+    url: "https://tolkiengateway.net/wiki/Lonely_Mountain",
+  },
+  mountains_of_angmar: {
+    name: "Mountains of Angmar",
+    elvish_name: "Emyn Angmar",
+    elvish_meaning: 'Emyn means "hills", and Angmar is composed of ang meaning "iron" and mar, a lenited form of bar meaning "dwelling."',
+    description: "The Mountains of Angmar were an extension of the Misty Mountains that lay to the west of Mount Gundabad, home to the Witch-king of Angmar who ruled from his capital Carn Dûm on the western tip of the mountain range.",
+    url: "https://tolkiengateway.net/wiki/Mountains_of_Angmar",
+  },
+};
