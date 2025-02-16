a817fc81485900cb144bb1373a46a816609192e6
jimrudolph726 <jimrudolph726@gmail.com>
2025-02-16 15:05:59 -0600

    d

--- functions.js
+++ functions.js
@@ -146,7 +146,7 @@ export const createMarkers = (locations, campsite = 'no') => {
 export const createGeographicFeature = async (geographic_data) => {
   const polygons = {};
   const promises = Object.keys(geographic_data).map(async (key) => {
-    const { pathName, color, name, PopupContent, tolerance = 1, weight } = geographic_data[key];
+    const { pathName, color, name, PopupContent, tolerance, weight } = geographic_data[key];
     const geojsonPath = `https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/geojson_files/${pathName}.geojson`;
 
     try {
