037cd52269d9e49a83e87c6354f7f8205799d37a
jimrudolph726 <jimrudolph726@gmail.com>
2025-02-16 12:31:44 -0600

    f

--- script.js
+++ script.js
@@ -41,15 +41,10 @@ markersData.forEach(({ data, checkboxId, campsite }) => {
   });
 });
 
-// Add Paths and Rivers
+// Add Paths
   createPolyline(pathdata).then((polylines) => {
   PathListeners(polylines, map);
   });
-// riverData.forEach(({ data, checkboxId }) => {
-//   createPolyline(data).then((polylines) => {
-//   MarkerListeners(checkboxId, polylines, map);
-//   });
-// });
 
 // Add Geographic Features
 geographicData.forEach(({ data, checkboxId }) => {
