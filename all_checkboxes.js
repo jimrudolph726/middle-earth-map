394af30584433558b893e104468a5dd6c269d389
jimrudolph726 <jimrudolph726@gmail.com>
2025-02-23 19:55:50 -0600

    d

--- all_checkboxes.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("allItemCheckbox").addEventListener("change", function () {
-    let itemCheckboxes = document.querySelectorAll("#itemsSection input.itemCheckbox");
-
-    itemCheckboxes.forEach(checkbox => {
-        checkbox.checked = this.checked;
-        checkbox.dispatchEvent(new Event("change"));  // Ensures MarkerListeners function runs
-    });
-});
