0779ae5944fc98ec64944f887d85665cc8bde37e
jimrudolph726 <jimrudolph726@gmail.com>
2025-02-25 19:47:16 -0600

    d

--- variables.js
+++ variables.js
@@ -294,19 +294,22 @@ Year3018March29: { coords: [44.947682267986565, -93.303630070285379], icon: icon
 }
 
 // Settlements (markers)
+// Settlements (markers)
 export const settlementsData = [
-  ...['samfrodo', 'aragorn', 'pippin', 'merry', 'gandalfthegrey'].map(name => ({
-    data: eval(`${name}campsites`),  // Dynamically fetch variable (ensure safety)
-    checkboxId: `${name}campsitesCheckbox`,
+  ...['samfrodocampsites', 'aragorncampsites', 'pippincampsites', 'merrycampsites', 'gandalfthegreycampsites'].map(name => ({
+    data: window[name],  // Safer than eval
+    checkboxId: `${name}Checkbox`,
     campsite: 'campsite'
   })),
 
   ...['elves', 'men', 'hobbits', 'battles', 'one_on_one', 'swords', 'rings', 'books'].map(name => ({
-    data: eval(name),  // Dynamically fetch variable (ensure safety)
+    data: window[name],  // Safer than eval
     checkboxId: `${name}Checkbox`,
     campsite: 'no'
   }))
 ];
+
+
 
 // Paths
 export const pathdata = { 
