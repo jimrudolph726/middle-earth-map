12b3e5fd6876056993d6f0253d2b3f5fed6b9ecf
jimrudolph726 <jimrudolph726@gmail.com>
2025-02-26 20:04:27 -0600

    d

--- geographic_data.js
+++ geographic_data.js
@@ -721,6 +721,7 @@ export const lakes_seas = {
     name: "Nen Hithoel",
     PopupContent: createGeographicPopup(
       "Nen Hithoel",
+      "Nen Hithoel",
       `Nen Hithoel means "mist-cool water" in Sindarin. The word nen means "water" and hîth means "mist." The final element oel apparently means "cool" although this usage is not found elsewhere.`,
       `Lake formed by the Anduin. Nen Hithoel was a long, oval-shaped lake. The rugged land called the Emyn Muil spread out from both shores of Nen Hithoel.`,
       "https://thainsbook.minastirith.cz/lakes.html#Hithoel"
@@ -734,6 +735,7 @@ export const lakes_seas = {
     name: "Sea of Nurnen",
     PopupContent: createGeographicPopup(
       "Sea of Nurnen",
+      "Nurnen",
       `On Tolkien's map of Middle-earth, this body of water is called the Sea of Nurnen. In the text, it is referred to as an inland sea but it is called Lake Nurnen. Nurnen means "sad water" in Sindarin. The word nurn means "lament" and the word nen means "water."`,
       `Body of water in Mordor. Nurnen was a large, dark lake or inland sea in the southeastern part of Mordor.`,
       "https://thainsbook.minastirith.cz/lakes.html#Lake%20Nurnen"
@@ -760,8 +762,23 @@ export const lakes_seas = {
     name: "Lake Helevorn",
     PopupContent: createGeographicPopup(
       "Lake Helevorn",
+      "Helevorn",
       `The name Helevorn means "black glass" in Sindarin. The Sindarin word hele or heledh meaning "glass" is derived from the Dwarvish word kheled. The element vorn is from morn meaning "black."`,
       `Lake in Beleriand. Lake Helevorn was located in the region of Thargelion on the western side of the Blue Mountains. It was a deep, dark lake at the southern foot of Mount Rerir, which stood out from the main range. `,
+      "https://thainsbook.minastirith.cz/lakes.html#Lake-Helevorn"
+    ),
+    tolerance: 1,
+    weight: 2
+  },
+  long_lake: {
+    pathName: 'long_lake',
+    color: "blue", 
+    name: "Long Lake",
+    PopupContent: createGeographicPopup(
+      "Long Lake",
+      "None",
+      `The name Helevorn means "black glass" in Sindarin. The Sindarin word hele or heledh meaning "glass" is derived from the Dwarvish word kheled. The element vorn is from morn meaning "black."`,
+      `Lake in Wilderland. Long Lake was formed by the River Running south of its source in the Lonely Mountain. It was a very large, oval-shaped lake that had once been a deep rocky valley.`,
       "https://thainsbook.minastirith.cz/lakes.html#Lake-Helevorn"
     ),
     tolerance: 1,
