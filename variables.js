b9dd1808bb5aedbd1b775ed9032cb7ab5d2e223a
jimrudolph726 <jimrudolph726@gmail.com>
2025-02-26 19:10:55 -0600

    d

--- variables.js
+++ variables.js
@@ -33,6 +33,19 @@ import {
   gandalfthegreycampsites,
  } from './campsite_data.js';
 
+// Map
+export const imageUrl = 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/middle-earth.png';
+export const map = L.map('map', {
+crs: L.CRS.EPSG3857,
+minZoom: 15,
+maxZoom: 20,
+zoom: 15.5, // Fractional zoom level
+center: [0, 0],
+zoomSnap: 1, // Allows fractional zoom levels
+zoomDelta: 5, // Controls the increment of zoom changes
+preferCanvas: true
+});
+export const imageBounds = [[44.95133395351252, -93.31776393673807],[44.93460911676505, -93.29255872642499],];
 // Campsites and Settlements
 export const settlementsData = [
   ...['samfrodocampsites', 'aragorncampsites', 'pippincampsites', 'merrycampsites', 'gandalfthegreycampsites'].map(name => ({
