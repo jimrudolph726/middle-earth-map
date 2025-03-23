5a59aa1d2fe0c974c82640996c34f0238f601f0b
jimrudolph726 <jimrudolph726@gmail.com>
2025-03-23 07:49:44 -0500

    d

--- beleriand/geographic_data.js
+++ beleriand/geographic_data.js
@@ -67,86 +67,16 @@ export const rivers = {
   },
 };
 export const lakes_seas = {
-  lake_evendim: {
-    pathName: 'lake_evendim',
+  lake_mithrim: {
+    pathName: 'lake_mithrim',
     color: "blue", 
-    name: "Lake Evendim",
+    name: "Lake Mithrim",
     PopupContent: createGeographicPopup(
-      "Lake Evendim",
-      "Evendim",
-      `Evendim means "evening twilight." The Sindarin name was Nenuial meaning "lake of twilight" from nen meaning "lake" and uial meaning "twilight."`,
-      `Lake in Eriador. Lake Evendim was in the Hills of Evendim north of the Shire. The lake was the source of the Brandywine River.`,
-      "https://thainsbook.minastirith.cz/lakes.html#Evendim"
-    ),
-    tolerance: 1,
-    weight: 2
-  },
-  nen_hithoel: {
-    pathName: 'nen_hithoel',
-    color: "blue", 
-    name: "Nen Hithoel",
-    PopupContent: createGeographicPopup(
-      "Nen Hithoel",
-      "Nen Hithoel",
-      `Nen Hithoel means "mist-cool water" in Sindarin. The word nen means "water" and hîth means "mist." The final element oel apparently means "cool" although this usage is not found elsewhere.`,
-      `Lake formed by the Anduin. Nen Hithoel was a long, oval-shaped lake. The rugged land called the Emyn Muil spread out from both shores of Nen Hithoel.`,
-      "https://thainsbook.minastirith.cz/lakes.html#Hithoel"
-    ),
-    tolerance: 1,
-    weight: 2
-  },
-  sea_of_nurnen: {
-    pathName: 'sea_of_nurnen',
-    color: "blue", 
-    name: "Sea of Nurnen",
-    PopupContent: createGeographicPopup(
-      "Sea of Nurnen",
-      "Nurnen",
-      `On Tolkien's map of Middle-earth, this body of water is called the Sea of Nurnen. In the text, it is referred to as an inland sea but it is called Lake Nurnen. Nurnen means "sad water" in Sindarin. The word nurn means "lament" and the word nen means "water."`,
-      `Body of water in Mordor. Nurnen was a large, dark lake or inland sea in the southeastern part of Mordor.`,
-      "https://thainsbook.minastirith.cz/lakes.html#Lake%20Nurnen"
-    ),
-    tolerance: 1,
-    weight: 2
-  },
-  sea_of_rhun: {
-    pathName: 'sea_of_rhun',
-    color: "blue", 
-    name: "Sea of Rhun",
-    PopupContent: createGeographicPopup(
-      "Sea of Rhun",
-      "Rhun",
-      `Also called the Inland Sea. Rhûn means "east" in Sindarin.`,
-      `Sea in eastern Middle-earth. The Sea of Rhun was a large inland sea located in Rhun, a land in far eastern Middle-earth.`,
-      "https://thainsbook.minastirith.cz/lakes.html#Rhun"
-    ),
-    tolerance: 1,
-    weight: 2
-  },
-  lake_helevorn: {
-    pathName: 'lake_helevorn',
-    color: "blue", 
-    name: "Lake Helevorn",
-    PopupContent: createGeographicPopup(
-      "Lake Helevorn",
-      "Helevorn",
-      `The name Helevorn means "black glass" in Sindarin. The Sindarin word hele or heledh meaning "glass" is derived from the Dwarvish word kheled. The element vorn is from morn meaning "black."`,
-      `Lake in Beleriand. Lake Helevorn was located in the region of Thargelion on the western side of the Blue Mountains. It was a deep, dark lake at the southern foot of Mount Rerir, which stood out from the main range. `,
-      "https://thainsbook.minastirith.cz/lakes.html#Lake-Helevorn"
-    ),
-    tolerance: 1,
-    weight: 2
-  },
-  long_lake: {
-    pathName: 'long_lake',
-    color: "blue", 
-    name: "Long Lake",
-    PopupContent: createGeographicPopup(
-      "Long Lake",
-      "None",
-      `The name Helevorn means "black glass" in Sindarin. The Sindarin word hele or heledh meaning "glass" is derived from the Dwarvish word kheled. The element vorn is from morn meaning "black."`,
-      `Lake in Wilderland. Long Lake was formed by the River Running south of its source in the Lonely Mountain. It was a very large, oval-shaped lake that had once been a deep rocky valley.`,
-      "https://thainsbook.minastirith.cz/lakes.html#Lake-Helevorn"
+      "Lake Mithrim",
+      "Mithrim",
+      `The name Mithrim means "Grey-elves" in Sindarin from mith meaning "grey" and rim meaning "host, great number."`,
+      `Lake in the region of Mithrim in southeastern Hithlum. Lake Mithrim was a long lake fed by four streams that came down from the mountains bordering Mithrim - three from the Ered Wethrin and one from the Mountains of Mithrim. `,
+      "https://thainsbook.minastirith.cz/lakes.html#Lake-Mithrim"
     ),
     tolerance: 1,
     weight: 2
