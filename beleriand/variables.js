02c941de0aab5234d4d2d252f8641824d895633b
jimrudolph726 <jimrudolph726@gmail.com>
2025-03-23 10:11:21 -0500

    d

--- beleriand/variables.js
+++ beleriand/variables.js
@@ -62,21 +62,9 @@ export const settlementsData = [
 
 // Paths
 export const pathdata = { 
-  samfrodopath: { 
-    pathName: 'samfrodopath', color: 'red', name: 'Sam and Frodo', PopupContent: createSettlementPopup('Sam and Frodo', '1766 miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5
+  beren_path: { 
+    pathName: 'beren_path', color: 'red', name: 'Beren', PopupContent: createSettlementPopup('Beren', ' ', 'https://tolkiengateway.net/wiki/Beren'), tolerance: 10, weight: 5
   }, 
-  aragornpath: { 
-    pathName: 'aragornpath', color: 'blue', name: 'Aragorn, Gimli and Legolas', PopupContent: createSettlementPopup('Aragorn, Gimli, and Legolas', '1575 miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5 
-  },
-  pippinpath: { 
-    pathName: 'pippinpath', color: 'green', name: 'Pippin', PopupContent: createSettlementPopup('Pippin', '855 miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5 
-  },
-  merrypath: { 
-    pathName: 'merrypath', color: 'orange', name: 'Merry', PopupContent: createSettlementPopup('Merry', '907 miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5 
-  },
-  gandalfthegrey_merged: { 
-    pathName: 'gandalfthegrey_merged', color: 'grey', name: 'Gandalf the Grey', PopupContent: createSettlementPopup('Gandalf the Grey', 'many thounsands of miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5 
-  },
 }
 
 // Geographic Features
