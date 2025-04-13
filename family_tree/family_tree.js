524bcfcf49c272f489a2d95eb31bca10d2a42755
jimrudolph726 <jimrudolph726@gmail.com>
2025-04-12 19:55:57 -0500

    d

--- family_tree/family_tree.js
+++ family_tree/family_tree.js
@@ -53,9 +53,11 @@ family.on('field', function (sender, args) {
 
 family.load(
     [
-        { id: 1, pids: [2], gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/aragorn.png', name: 'Aragon Elessar II', born: 'March 1, 2931 of the Third Age', died: 'March 1, 120 of the Fourth Age' },
-        { id: 2, pids: [1], gender: 'female', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/arwen.png', name: 'Arwen Undómiel', born: '41 of the Third Age', died: '121 of the Fourth Age' },
-        { id: 3, mid: 2, fid: 1, gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/eldarion.png', name: 'Eldarion', born: 'Unknown', died: 'around 220 of the Fourth Age' },
+        { id: 1, pids: [2], gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/elrond.png', name: 'Elrond', born: 'Around 532 of the First Age', died: 'No death, left Middle-earth on September 29, 3021 of the Third Age' },
+        { id: 2, pids: [1], gender: 'female', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/celebrian.png', name: 'Celebrian', born: 'Likely born between S.A. 750 and S.A. 900', died: 'No death, left Middle-earth in 2510 of the Third Age' },
+        { id: 3, pids: [4], gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/aragorn.png', name: 'Aragon Elessar II', born: 'March 1, 2931 of the Third Age', died: 'March 1, 120 of the Fourth Age' },
+        { id: 4, pids: [3], mid: 2, fid: 1, gender: 'female', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/arwen.png', name: 'Arwen Undómiel', born: '41 of the Third Age', died: '121 of the Fourth Age' },
+        { id: 5, mid: 4, fid: 3, gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/eldarion.png', name: 'Eldarion', born: 'Unknown', died: 'Around 220 of the Fourth Age' },
     ]
 );
 
