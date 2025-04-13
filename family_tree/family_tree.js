0dbbab26c61fb8221388f19f544d1e760aa14cb6
jimrudolph726 <jimrudolph726@gmail.com>
2025-04-12 20:49:32 -0500

    d

--- family_tree/family_tree.js
+++ family_tree/family_tree.js
@@ -29,10 +29,6 @@ var family = new FamilyTree(document.getElementById('tree'), {
             { type: 'textbox', label: 'Full Name', binding: 'name' },
             { type: 'textbox', label: 'Date Of Birth', binding: 'born' },
             { type: 'textbox', label: 'Date Of Death', binding: 'died' },
-            [
-                { type: 'select', options: [{ value: 'bg', text: 'Bulgaria' }, { value: 'ru', text: 'Russia' }, { value: 'gr', text: 'Greece' }], label: 'Country', binding: 'country' },
-                { type: 'textbox', label: 'City', binding: 'city' },
-            ],
             { type: 'textbox', label: 'Photo Url', binding: 'photo', btn: 'Upload' },
         ]
     },
@@ -53,10 +49,11 @@ family.on('field', function (sender, args) {
 
 family.load(
     [
+        { id: 1, pids: [2], gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/elrond.png', name: 'Elrond', born: 'Around First Age 532', died: 'No death, left Middle-earth on September 29, 3021 of the Third Age' },
         { id: 1, pids: [2], gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/elrond.png', name: 'Elrond', born: 'Around 532 of the First Age', died: 'No death, left Middle-earth on September 29, 3021 of the Third Age' },
-        { id: 2, pids: [1], gender: 'female', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/celebrian.png', name: 'Celebrian', born: 'Likely born between S.A. 750 and S.A. 900', died: 'No death, left Middle-earth in 2510 of the Third Age' },
+        { id: 2, pids: [1], gender: 'female', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/celebrian.png', name: 'Celebrian', born: 'Likely born between Second Age 750 and Second Age 900', died: 'No death, left Middle-earth in 2510 of the Third Age' },
         { id: 3, pids: [4], gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/aragorn.png', name: 'Aragon Elessar II', born: 'March 1, 2931 of the Third Age', died: 'March 1, 120 of the Fourth Age' },
-        { id: 4, pids: [3], mid: 2, fid: 1, gender: 'female', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/arwen.png', name: 'Arwen Undómiel', born: '41 of the Third Age', died: '121 of the Fourth Age' },
+        { id: 4, pids: [3], mid: 2, fid: 1, gender: 'female', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/arwen.png', name: 'Arwen Undómiel', born: 'Third Age 41', died: 'Fourth Age 121' },
         { id: 5, mid: 4, fid: 3, gender: 'male', photo: 'https://raw.githubusercontent.com/jimrudolph726/middle-earth-map/main/assets/family_tree/eldarion.png', name: 'Eldarion', born: 'Unknown', died: 'Around 220 of the Fourth Age' },
     ]
 );
