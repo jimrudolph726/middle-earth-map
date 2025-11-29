ffba3c02bb5051b4ec0ba8a4fdd47a7156cc1809
jimrudolph726 <jimrudolph726@gmail.com>
2025-11-29 09:40:20 -0600

    d

--- beleriand/campsite_data.js
+++ beleriand/campsite_data.js
@@ -8,6 +8,6 @@ import {
 
   
 export const berencampsites = {
-    September23: { coords: [44.9415162,-93.2962679], icon: icons.TentIcon, popup: createCampsitePopup('September 23',5,18,3.6,'Evening march.','Green Hill Country'),
+    September23: { coords: [44.9428698,-93.2952427], icon: icons.TentIcon, popup: createCampsitePopup('FA 460-464',5,18,3.6,'Beren lives in Dorthonion during his youth.','Dorthonion'),
     },
   };
