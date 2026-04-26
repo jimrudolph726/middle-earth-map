dbe7ca5d76cc39a04016616aed05fe6a21ea8429
jimrudolph726 <jimrudolph726@gmail.com>
2026-04-26 17:09:43 -0500

    d

--- variables.js
+++ variables.js
@@ -135,8 +135,11 @@ export const pathdata = {
   merrypath: { 
     pathName: 'merrypath', color: 'orange', name: 'Merry', PopupContent: createSettlementPopup('Merry', '907 miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5 
   },
-  gandalfthegrey: { 
-    pathName: 'gandalfthegrey', color: 'grey', name: 'Gandalf the Grey', PopupContent: createSettlementPopup('Gandalf the Grey', 'many thounsands of miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5, arrows: true 
+  gandalfthegreypath: { 
+    pathName: 'gandalfthegreypath', color: 'grey', name: 'Gandalf the Grey', PopupContent: createSettlementPopup('Gandalf the Grey', 'many thounsands of miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5, arrows: true 
+  },
+  gandalfthewhitepath: { 
+    pathName: 'gandalfthewhitepath', color: 'white', name: 'Gandalf the White', PopupContent: createSettlementPopup('Gandalf the White', 'many thounsands of miles', 'https://tolkiengateway.net/wiki/Quest_of_the_Ring'), tolerance: 10, weight: 5, arrows: true 
   },
 }
 
