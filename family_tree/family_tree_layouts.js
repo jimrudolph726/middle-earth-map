a408b3e6df24540a15417680d1f7ad5d93c1f42f
jimrudolph726 <jimrudolph726@gmail.com>
2026-05-03 18:54:29 -0500

    d

--- family_tree/family_tree_layouts.js
+++ family_tree/family_tree_layouts.js
@@ -1,6 +1,73 @@
-(function () {
-  window.FAMILY_TREE_LAYOUTS = window.FAMILY_TREE_LAYOUTS || {
-    version: 1,
-    views: {}
-  };
-}());
+window.FAMILY_TREE_LAYOUTS = {
+  "version": 1,
+  "views": {
+    "aragorn_lineage": {
+      "positions": {
+        "elros": {
+          "x": 352,
+          "y": 288
+        },
+        "arathorn_second": {
+          "x": 353,
+          "y": 713
+        },
+        "gilraen": {
+          "x": 619,
+          "y": 696
+        },
+        "arwen": {
+          "x": 826,
+          "y": 900
+        },
+        "aragorn_second": {
+          "x": 484,
+          "y": 896
+        },
+        "earendil": {
+          "x": 200,
+          "y": 84
+        },
+        "elwing": {
+          "x": 488,
+          "y": 76
+        },
+        "arador": {
+          "x": 348,
+          "y": 508
+        },
+        "unknown_daughters": {
+          "x": 545,
+          "y": 1124
+        },
+        "eldarion": {
+          "x": 261,
+          "y": 1116
+        }
+      }
+    },
+    "all_lineages": {
+      "positions": {
+        "aragorn_second": {
+          "x": 206,
+          "y": 961
+        },
+        "arwen": {
+          "x": 592,
+          "y": 891
+        },
+        "gilraen": {
+          "x": 340,
+          "y": 782
+        },
+        "arathorn_second": {
+          "x": 73,
+          "y": 772
+        },
+        "celebrian": {
+          "x": 845,
+          "y": 291
+        }
+      }
+    }
+  }
+};
