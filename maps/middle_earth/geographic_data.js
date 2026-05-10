ce3745b743038f9dc20f0741f05c08ca4ee1f866
jimrudolph726 <jimrudolph726@gmail.com>
2026-05-10 17:19:08 -0500

    d

--- maps/middle_earth/geographic_data.js
+++ maps/middle_earth/geographic_data.js
@@ -1117,6 +1117,7 @@ export const islands = {
     PopupContent: createGeographicPopup(
     "Cair Andros",
     "Cair Andros",
+    `Cair Andros means "ship of long foam," in reference to the island's shape and the foamy waters that broke on its northern end. The word cair means "ship." The element and means "long" and ros means "foam, spray."`
     `Island in the Anduin. Cair Andros was located in the middle of the river about 30 miles north of Osgiliath. There were woods on Cair Andros. The island was long and narrow and was shaped like a ship.`,
     "https://thainsbook.minastirith.cz/coastal.html#Cair_Andros"),
     tolerance: 1,
@@ -1129,6 +1130,7 @@ export const islands = {
     PopupContent: createGeographicPopup(
     "Tolfalas",
     "Tolfalas",
+    `Also written as Tol Falas. The name means "coastal island" from tol meaning "island with sheer sides" and falas meaning "shore, coast."`
     `Island in the Bay of Belfalas. Tolfalas was located in an inlet at the Mouths of the Anduin. It was a large island, over 50 miles in length. There were hills or mountains in the central region of Tolfalas.`,
     "https://tolkiengateway.net/wiki/Tolfalas"),
     tolerance: 1,
@@ -1141,6 +1143,9 @@ export const islands = {
     PopupContent: createGeographicPopup(
     "Tindrock",
     "Tol Brandir",
+    `The name Tindrock means "spiked rock." The word tind is Old English meaning "tine, spike, prong." Also called Tindrock Isle.
+
+Called Tol Brandir in Sindarin. The word tol means "island with sheer sides." The word brandir is said to be a corruption of baradnir meaning "tower-steep" or "steep tower." Barad means "tower" and there is also an adjective baradh meaning "steep" but the meaning of nir is unclear.`
     `Island in Nen Hithoel on the Anduin. The Tindrock was at the southern end of the lake above the Falls of Rauros. On the western shore just south of the Tindrock stood Amon Hen, the Hill of Sight, and on the eastern shore was Amon Lhaw, the Hill of Hearing.`,
     "https://thainsbook.minastirith.cz/coastal.html#Tindrock"),
     tolerance: 1,
