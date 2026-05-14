7373f22f5a570729b6ebf5ca5358e2c56ff435f3
jimrudolph726 <jimrudolph726@gmail.com>
2026-05-13 20:04:35 -0500

    d

--- maps/middle_earth/script.js
+++ maps/middle_earth/script.js
@@ -159,6 +159,7 @@ const checkboxMappings = {
   allRegionCheckbox: "#regionsSection input.regionCheckbox",
   allRoadCheckbox: "#roadsSection input.roadCheckbox",
   allProvisionCheckbox: "#provisionsSection input.provisionCheckbox",
+  allCreaturesAndBeingsCheckbox: "#provisionsSection input.provisionCheckbox",
 };
 Object.keys(checkboxMappings).forEach(masterCheckboxId => {
   const masterCheckbox = document.getElementById(masterCheckboxId);
